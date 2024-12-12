5ea8a4079b978a803bd80d805b6b34f590d96e84
Omasta-Parisot-Ethan <ethan.omasta-parisot@edu.univ-fcomte.fr>
2024-12-12 18:49:38 +0100

    commit modification des boutons et mise en place du menu en html // Mise en place de certains composants en css et en js (pas terminés)

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
-function togglemenu() {
-    var menu = document.getElementById('menu')
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none'
-    } else {
-        menu.style.display = 'block'
-    }
+var header__menu = document.getElementById("Myheader__menu")
+var openbtn = document.getElementById("openbtn")
+var closebtn = document.getElementById("closebtn")
+
+openbtn.addEventListener("click", openNav);
+closebtn.addEventListener("click", closeNav);
+
+function openNav() {
+    console.log("Ouverture du menu");
+    header__menu.classList.add("active")
+}
+
+function closeNav() {
+    console.log("Fermeture du menu");
+    header__menu.classList.remove("active")
 }
