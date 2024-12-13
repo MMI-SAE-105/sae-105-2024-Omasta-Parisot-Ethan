5212ca409075e4e98ed4961cc60950b384fc4fc6
Omasta-Parisot-Ethan <ethan.omasta-parisot@edu.univ-fcomte.fr>
2024-12-13 09:54:40 +0100

    commit tentative du menu (non réussi)

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,46 @@
-var header__menu = document.getElementById("Myheader__menu")
-var openbtn = document.getElementById("openbtn")
-var closebtn = document.getElementById("closebtn")
+// menu //
 
-openbtn.addEventListener("click", openNav);
-closebtn.addEventListener("click", closeNav);
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerIcon = document.getElementById("burger-icon");
+    const closeBtn = document.getElementById("closebtn");
+    const menu = document.getElementById("header__menu");
+  
+    burgerIcon.addEventListener("click", () => {
+      menu.style.left = "0";
+    });
+  
+    closeBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      menu.style.left = "-250px";
+    });
+  });
 
-function openNav() {
-    console.log("Ouverture du menu");
-    header__menu.classList.add("active")
-}
-
-function closeNav() {
-    console.log("Fermeture du menu");
-    header__menu.classList.remove("active")
-}
+// carousel //
 
 document.addEventListener('DOMContentLoaded', () => {
-    const carousels = document.querySelectorAll('.carousel');
-})
+    const carousels = document.querySelectorAll('.carousel'); 
+
+    carousels.forEach(carousel => {
+        const carouselContainer = carousel.querySelector('.carousel-container');
+        const slides = carousel.querySelectorAll('.carousel-slide');
+        const prevButton = carousel.querySelector('.carousel-button.prev');
+        const nextButton = carousel.querySelector('.carousel-button.next');
+
+        let currentIndex = 0;
+
+        function updateCarousel() {
+            const offset = -currentIndex * 100; 
+            carouselContainer.style.transform = translateX({offset});
+        }
+
+        nextButton.addEventListener('click', () => {
+            currentIndex = (currentIndex + 1) % slides.length;
+            updateCarousel();
+        });
+
+        prevButton.addEventListener('click', () => {
+            currentIndex = (currentIndex - 1 + slides.length) % slides.length; 
+            updateCarousel();
+        });
+    });
+});
